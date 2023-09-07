5a12f93c4dba0c3da0ecd5429dc7d84405f36850
Kiridepapel <monalek0622@hotmail.com>
2023-09-07 15:46:35 -0500

    main(dev): main files
    main files for the backend application project

---
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/MonolithFoodApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.MonolithFoodApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithFoodApplication.class, args);
+	}
+
+}
