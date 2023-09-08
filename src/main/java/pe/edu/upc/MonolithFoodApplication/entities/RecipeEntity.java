70fa9fcdf461f5d66e97065047fefb1440307d7a
AlexDom <heatherxvalencia@gmail.com>
2023-09-08 18:30:04 -0500

    Update RecipeEntity.java

--- src/main/java/pe/edu/upc/MonolithFoodApplication/entities/RecipeEntity.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/entities/RecipeEntity.java
@@ -1,7 +1,42 @@
 package pe.edu.upc.MonolithFoodApplication.entities;
 
 // Heather
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor; 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+@Entity
+@Table(name = "recipes")
 public class RecipeEntity {
-    
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String name;
+  
+  private String description;
+
+  private String preparationGuide;
+  
+  private String benefits;
+  
+  private String disadvantages;
+
+  private Boolean isPrivate;
+
+  private Boolean isFavorite;
+
+  @ManyToOne
+  @JoinColumn(name = "creator_user_id")
+  private UserEntity creatorUser;
+
+  @ManyToMany(mappedBy = "recipes")
+  private List<FoodEntity> foods;
+  
 }
