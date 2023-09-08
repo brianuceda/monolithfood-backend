e62907949465c94d6b2876fb43e8f372ee863660
Gabriela Reyes <107740335+GabrielaReyes19@users.noreply.github.com>
2023-09-08 15:39:42 -0500

    feat(dev): Eat, User, Food

--- src/main/java/pe/edu/upc/MonolithFoodApplication/entities/EatEntity.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/entities/EatEntity.java
@@ -1,7 +1,36 @@
 package pe.edu.upc.MonolithFoodApplication.entities;
 
 // Gabriela
+import jakarta.persistence.*;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "eat")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class EatEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private FoodEntity food;
+
+    private int quantity; 
+    private String mealType; 
 }
--- src/main/java/pe/edu/upc/MonolithFoodApplication/entities/FoodEntity.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/entities/FoodEntity.java
@@ -2,6 +2,32 @@ package pe.edu.upc.MonolithFoodApplication.entities;
 
 // Brian | Gabriela | Heather
 
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "food")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class FoodEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    private String name;
+    private String description;
+    private int quantity;
+    private boolean isPrivate;
+    private boolean isFavorite;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_user_id")
+    private UserEntity creatorUser;
+   
 }
