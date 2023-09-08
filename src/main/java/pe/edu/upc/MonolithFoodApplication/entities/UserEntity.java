4e4d3c519f527c290bb6505649d46c4bc6be8aeb
AlexDom <heatherxvalencia@gmail.com>
2023-09-08 18:55:38 -0500

    ya

--- src/main/java/pe/edu/upc/MonolithFoodApplication/entities/UserEntity.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/entities/UserEntity.java
@@ -2,6 +2,36 @@ package pe.edu.upc.MonolithFoodApplication.entities;
 
 // Gabriela | Heather | Naydeline | Willy
 
+import jakarta.persistence.*;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String names;
+    private String surnames;
+    private String profileImg;
+    private Boolean isPrivate;
+    private Boolean isFavorite;
+
+     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<EatEntity> eats;
+
+    @OneToMany(mappedBy = "creatorUser", cascade = CascadeType.ALL)
+    private List<FoodEntity> createdFoods;
+
+
 }
