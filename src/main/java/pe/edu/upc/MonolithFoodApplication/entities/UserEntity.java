94b50624564fb644fe8aec0c20782d7355dcf654
Gabriela Reyes <107740335+GabrielaReyes19@users.noreply.github.com>
2023-09-08 20:08:06 -0500

    Update UserEntity.java

--- src/main/java/pe/edu/upc/MonolithFoodApplication/entities/UserEntity.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/entities/UserEntity.java
@@ -1,35 +1,47 @@
 package pe.edu.upc.MonolithFoodApplication.entities;
 
-// Gabriela | Heather | Naydeline | Willy
+import java.util.List;
 
 import jakarta.persistence.*;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// Gabriela | Heather | Naydeline | Willy
 
-@Entity
-@Table(name = "user")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class UserEntity {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
+    @Column(nullable = false, length = 50, unique = true)
     private String username;
+
+    @Column(nullable = false, length = 80)
     private String password;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 100)
     private String names;
+
+    @Column(nullable = false, length = 100)
     private String surnames;
+
+    @Column(nullable = false, length = 755)
     private String profileImg;
 
-     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<EatEntity> eats;
 
     @OneToMany(mappedBy = "creatorUser", cascade = CascadeType.ALL)
     private List<FoodEntity> createdFoods;
-
 
 }
