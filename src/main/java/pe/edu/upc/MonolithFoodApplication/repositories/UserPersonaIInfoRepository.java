8b1e998cd39c07ad578f6ce469f4ca50f4813b47
Naydeline Duran <107515912+Nayde13@users.noreply.github.com>
2023-09-16 22:59:19 -0500

    2

--- src/main/java/pe/edu/upc/MonolithFoodApplication/repositories/UserPersonaIInfoRepository.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/repositories/UserPersonaIInfoRepository.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.MonolithFoodApplication.repositories;
 
-public interface UserPersonaIInfoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.MonolithFoodApplication.entities.UserPersonalInfoEntity;
+
+@Repository
+public interface UserPersonaIInfoRepository extends JpaRepository<UserPersonalInfoEntity,Long> {
+
 }
---
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/services/UserPersonalInfoService.java
@@ -0,0 +1,19 @@
+package pe.edu.upc.MonolithFoodApplication.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.MonolithFoodApplication.entities.UserPersonalInfoEntity;
+import pe.edu.upc.MonolithFoodApplication.repositories.UserPersonaIInfoRepository;
+
+@Service
+public class UserPersonalInfoService {
+
+    @Autowired
+    private final UserPersonaIInfoRepository userPersonaIInfoRepository;
+    public UserPersonalInfoService(UserPersonaIInfoRepository userPersonaIInfoRepository) {
+        this.userPersonaIInfoRepository = userPersonaIInfoRepository;
+    }
+
+    public UserPersonalInfoEntity
+
+}
