be107cd0c481c59f6309b4d96665acdcdf408765
Naydeline Duran <107515912+Nayde13@users.noreply.github.com>
2023-09-16 22:38:21 -0500

    Update DtoProfileEdit.java

--- src/main/java/pe/edu/upc/MonolithFoodApplication/dtos/DtoProfileEdit.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/dtos/DtoProfileEdit.java
@@ -1,4 +1,25 @@
 package pe.edu.upc.MonolithFoodApplication.dtos;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pe.edu.upc.MonolithFoodApplication.entities.ActivityLevelEntity;
+import pe.edu.upc.MonolithFoodApplication.entities.GenderEnum;
+import pe.edu.upc.MonolithFoodApplication.entities.UserEntity;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DtoProfileEdit {
+
+    private Long id;
+    private GenderEnum gender;
+    private Date birthdate;
+    private Double heightCm;
+    private Double weightKg;
+    private ActivityLevelEntity activityLevel;
+    private UserEntity user;
+
 }
