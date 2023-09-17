70e1e0e1d1615daa98e7b8c4d93954b5fe4fc250
Naydeline Duran <107515912+Nayde13@users.noreply.github.com>
2023-09-16 23:31:26 -0500

    Update UserPersonalInfoController.java

--- src/main/java/pe/edu/upc/MonolithFoodApplication/controllers/UserPersonalInfoController.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/controllers/UserPersonalInfoController.java
@@ -1,10 +1,29 @@
 package pe.edu.upc.MonolithFoodApplication.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.MonolithFoodApplication.dtos.UserPersonallnfoDto;
+import pe.edu.upc.MonolithFoodApplication.entities.UserPersonalInfoEntity;
+import pe.edu.upc.MonolithFoodApplication.services.UserPersonalInfoService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/userpersonalInfo")
 public class UserPersonalInfoController {
+
+    @Autowired
+    private final UserPersonalInfoService userPersonalInfoService;
+    public UserPersonalInfoController(UserPersonalInfoService userPersonalInfoService) {
+        this.userPersonalInfoService = userPersonalInfoService;
+    }
+
+    @PutMapping
+    public ResponseEntity<UserPersonallnfoDto> updatePersonalInfo (@RequestBody UserPersonallnfoDto userPersonallnfoDto){
+        return new ResponseEntity<>(userPersonalInfoService.updateUserPeronalInfo(userPersonallnfoDto), HttpStatus.OK);
+    }
+
 }
