9f157fb7a54d05ec40b60674441cceac9db2a33b
Kiridepapel <monalek0622@hotmail.com>
2023-09-24 22:18:22 -0500

    Naydeline PR (#11)

---
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/controllers/UserPersonalInfoController.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.MonolithFoodApplication.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+import pe.edu.upc.MonolithFoodApplication.dtos.general.ResponseDTO;
+import pe.edu.upc.MonolithFoodApplication.dtos.userpersonal.PersonalInfoRequestDTO;
+import pe.edu.upc.MonolithFoodApplication.services.UserPersonalInfoService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/personal-info")
+public class UserPersonalInfoController {
+    // * Atributos
+    // Inyección de dependencias
+    private final UserPersonalInfoService userPersonalInfoService;
+
+    // * Metodos
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDTO>updatePersonalInfo(@RequestParam String username, @RequestBody PersonalInfoRequestDTO userPersonallnfoDto) {
+        ResponseDTO response = userPersonalInfoService.updateUserPeronalInfo(username, userPersonallnfoDto);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatusCode()));
+    }
+
+}
---
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/dtos/userpersonal/PersonalInfoResponseDTO.java
@@ -0,0 +1,20 @@
+package pe.edu.upc.MonolithFoodApplication.dtos.userpersonal;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import pe.edu.upc.MonolithFoodApplication.dtos.general.ResponseDTO;
+
+@Getter
+@Setter
+public class PersonalInfoResponseDTO extends ResponseDTO {
+
+    private PersonalInfoRequestDTO updatedPersonalInfo;
+
+    @Builder
+    public PersonalInfoResponseDTO(String message, Integer statusCode, PersonalInfoRequestDTO updatedPersonalInfo) {
+        super(message, statusCode);
+        this.updatedPersonalInfo = updatedPersonalInfo;
+    }
+    
+}
