1b5fe496df4145c4357bfa67948b6209065b60e8
Kiridepapel <monalek0622@hotmail.com>
2023-09-24 22:22:40 -0500

    feat(dev): replace username form token.username

--- src/main/java/pe/edu/upc/MonolithFoodApplication/controllers/UserConfigController.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/controllers/UserConfigController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import pe.edu.upc.MonolithFoodApplication.dtos.general.ResponseDTO;
 import pe.edu.upc.MonolithFoodApplication.dtos.userconfig.UserConfigDTO;
+import pe.edu.upc.MonolithFoodApplication.services.JwtService;
 import pe.edu.upc.MonolithFoodApplication.services.UserConfigService;
 
 @RestController
@@ -20,20 +22,24 @@ public class UserConfigController {
     // * Atributos
     // Inyección de dependencias
     private final UserConfigService userConfigService;
+    private final JwtService jwtService;
 
     // * Metodos
     @GetMapping("/getConfig")
-    public ResponseEntity<?> getConfig(@RequestParam String username) {
+    public ResponseEntity<?> getConfig(@RequestHeader("Authorization") String bearerToken) {
+        String username = jwtService.getUsernameFromBearerToken(bearerToken);
         UserConfigDTO response = userConfigService.getConfig(username);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatusCode()));
     }
     @PutMapping("/darkmode")
-    public ResponseEntity<?> changeDarkMode(@RequestParam String username, @RequestParam Boolean darkMode) {
+    public ResponseEntity<?> changeDarkMode(@RequestHeader("Authorization") String bearerToken, @RequestParam Boolean darkMode) {
+        String username = jwtService.getUsernameFromBearerToken(bearerToken);
         ResponseDTO response = userConfigService.changeDarkMode(username, darkMode);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatusCode()));
     }
     @PutMapping("/notifications")
-    public ResponseEntity<?> changeNotifications(@RequestParam String username, @RequestParam Boolean notifications) {
+    public ResponseEntity<?> changeNotifications(@RequestHeader("Authorization") String bearerToken, @RequestParam Boolean notifications) {
+        String username = jwtService.getUsernameFromBearerToken(bearerToken);
         ResponseDTO response = userConfigService.changeNotifications(username, notifications);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatusCode()));
     }
--- src/main/java/pe/edu/upc/MonolithFoodApplication/controllers/UserPersonalInfoController.java
+++ src/main/java/pe/edu/upc/MonolithFoodApplication/controllers/UserPersonalInfoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 import pe.edu.upc.MonolithFoodApplication.dtos.general.ResponseDTO;
 import pe.edu.upc.MonolithFoodApplication.dtos.userpersonal.PersonalInfoRequestDTO;
+import pe.edu.upc.MonolithFoodApplication.services.JwtService;
 import pe.edu.upc.MonolithFoodApplication.services.UserPersonalInfoService;
 
 @RestController
@@ -16,10 +17,12 @@ public class UserPersonalInfoController {
     // * Atributos
     // Inyección de dependencias
     private final UserPersonalInfoService userPersonalInfoService;
+    private final JwtService jwtService;
 
     // * Metodos
     @PutMapping("/update")
-    public ResponseEntity<ResponseDTO>updatePersonalInfo(@RequestParam String username, @RequestBody PersonalInfoRequestDTO userPersonallnfoDto) {
+    public ResponseEntity<ResponseDTO>updatePersonalInfo(@RequestHeader("Authorization") String bearerToken, @RequestBody PersonalInfoRequestDTO userPersonallnfoDto) {
+        String username = jwtService.getUsernameFromBearerToken(bearerToken);
         ResponseDTO response = userPersonalInfoService.updateUserPeronalInfo(username, userPersonallnfoDto);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatusCode()));
     }
