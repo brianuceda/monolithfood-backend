9c05cf056e017b7a05de3242c1b6abae27ec82d4
Brian Uceda <u202114785@upc.edu.pe>
2025-04-23 00:22:00 -0500

    testing docker-compose

---
+++ src/main/java/com/example/monolithfood_backend/MonolithfoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.monolithfood_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithfoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithfoodBackendApplication.class, args);
+	}
+
+}
