12ceac2dd8c10934c23772772bbc4e4c7f23a442
Brian Uceda <u202114785@upc.edu.pe>
2025-04-23 09:41:26 -0500

    testing env variables and dockerfile

--- src/main/java/com/example/monolithfood_backend/controllers/TestController.java
+++ src/main/java/com/example/monolithfood_backend/controllers/TestController.java
@@ -3,6 +3,7 @@ package com.example.monolithfood_backend.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/test")
 public class TestController {
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+    
+    @Value("${jwt.expiration-time}")
+    private Long expirationTime;
 
     @GetMapping
     public ResponseEntity<Map<String, String>> test() {
         Map<String, String> response = new HashMap<>();
         response.put("message", "Hello World");
+        response.put("secretKey", secretKey);
+        response.put("expirationTime", expirationTime.toString());
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
